8739639bd9d684b6cdd9b1f62d0246535d4927cb
plbenveniste <plbenveniste@Sansfil-Securise-Employes-Poly-226-246.polymtl.ca>
2023-06-22 15:24:31 -0400

    Added folder for nnunet experiment

---
+++ nnunet/convert_bids_to_nnunet.py
@@ -0,0 +1,199 @@
+# This scripts was adapted from a script by the following authors : Julian McGinnis, Naga Karthik 
+#
+# This script is specifically designed to CanProCo dataset. It converts the dataset from a BIDS format to a nnU-Net format. 
+# 
+import argparse
+import pathlib
+from pathlib import Path
+import json
+import os
+import shutil
+from collections import OrderedDict
+from tqdm import tqdm
+
+import nibabel as nib
+import numpy as np
+
+def label_encoding(subject_path, label_path, label_gm_path, label_wm_path, threshold=1e-12):
+    #Encoding of the label images with background=0 , GM=1 and WM=2
+    #We consider that GM and WM don't overlap (maybe need to change that ?)
+
+    #For grey matter
+    label_gm_npy = nib.load(label_gm_path).get_fdata() 
+    label_gm_npy = np.where(label_gm_npy > threshold, 1, 0)
+    #For white matter
+    label_wm_npy = nib.load(label_wm_path).get_fdata() 
+    label_wm_npy = np.where(label_wm_npy > threshold, 2, 0)
+    
+    #Addition of both
+    label_npy = label_gm_npy + label_wm_npy
+    
+    ref = nib.load(subject_path)
+    label_bin = nib.Nifti1Image(label_npy, ref.affine, ref.header)
+    # overwrite the original label file with the binarized version
+    nib.save(label_bin, label_path)
+
+
+# parse command line arguments
+parser = argparse.ArgumentParser(description='Convert BIDS-structured database to nnUNet format.')
+parser.add_argument('--path-data', required=True,
+                    help='Path to BIDS structured dataset. Accepts both cross-sectional and longitudinal datasets')
+parser.add_argument('--path-out', help='Path to output directory.', required=True)
+parser.add_argument('--taskname', default='MSSpineLesion', type=str,
+                    help='Specify the task name - usually the anatomy to be segmented, e.g. Hippocampus',)
+parser.add_argument('--tasknumber', default=501,type=int, 
+                    help='Specify the task number, has to be greater than 500 but less than 999. e.g 502')
+parser.add_argument('--label-folder', help='Path to the label folders in derivatives', default='labels', type=str)
+#parser.add_argument('--mri-protocols', help='protocols to select', default='STIR,PSIR', type=str)
+
+args = parser.parse_args()
+
+path_in_images = Path(args.path_data)
+label_folder = args.label_folder
+path_in_labels = Path(os.path.join(args.path_data, 'derivatives', label_folder))
+path_out = Path(os.path.join(os.path.abspath(args.path_out), f'Dataset{args.tasknumber}_{args.taskname}'))
+#mri_protocols = args.mri_protocols.split(',')
+
+# define paths for train and test folders 
+path_out_imagesTr = Path(os.path.join(path_out, 'imagesTr'))
+path_out_imagesTs = Path(os.path.join(path_out, 'imagesTs'))
+path_out_labelsTr = Path(os.path.join(path_out, 'labelsTr'))
+path_out_labelsTs = Path(os.path.join(path_out, 'labelsTs'))
+
+# we load both train and validation set into the train images as nnunet uses cross-fold-validation
+train_images, train_labels = [], []
+test_images, test_labels = [], []
+
+if __name__ == '__main__':
+
+    # make the directories
+    pathlib.Path(path_out).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(path_out_imagesTr).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(path_out_imagesTs).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(path_out_labelsTr).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(path_out_labelsTs).mkdir(parents=True, exist_ok=True)
+
+    conversion_dict = {}
+    
+
+    #------------- EXTRACTION OF THE LABELED IMAGES NAMES--------------------------
+    labelled_imgs = []
+    
+    # We first extract all the label files' names
+    
+    #label_dirs = sorted(list(path_in_labels.glob('*/')))
+    
+    label_files = sorted(list(path_in_labels.rglob('*_STIR_lesion-manual.nii.gz')) + list(path_in_labels.rglob('*PSIR_lesion-manual.nii.gz') ))
+    labelled_imgs += [str(k) for k in label_files]
+    
+    #--------------- DISPACTH OF LABELLED IMAGES AND UNLABELED IMAGES ------------------- 
+    
+    #Initialise the number of scans in train and in test folder
+    scan_cnt_train, scan_cnt_test = 0, 0
+
+    valid_train_imgs = []
+    valid_test_imgs = []
+
+    #The image folders
+    #dirs = sorted(list(path_in_images.glob('*/')))
+    #dirs = [str(x) for x in dirs]
+
+    image_files = sorted(list(path_in_images.rglob('*_STIR.nii.gz')) + list(path_in_images.rglob('*_PSIR.nii.gz')))
+    
+    for image_file in image_files:
+
+        #Identify common data path
+        common = os.path.relpath(os.path.dirname(image_file), args.path_data)
+
+        file_id = str(image_file).rsplit('_',1)[0].split('/')[-1] + "_"
+        
+        if (file_id in str(labelled_imgs)):
+            label_file = [k for k in labelled_imgs if file_id in k][0]
+
+            scan_cnt_train+= 1
+
+            image_file_nnunet = os.path.join(path_out_imagesTr,f'{args.taskname}_{scan_cnt_train:03d}_0000.nii.gz')
+            label_file_nnunet = os.path.join(path_out_labelsTr,f'{args.taskname}_{scan_cnt_train:03d}.nii.gz')
+            
+            train_images.append(str(image_file_nnunet))
+            train_labels.append(str(label_file_nnunet))
+            
+            # copy the files to new structure
+            shutil.copyfile(image_file, image_file_nnunet)
+            shutil.copyfile(label_file, label_file_nnunet)
+           
+            conversion_dict[str(os.path.abspath(image_file))] = image_file_nnunet
+            conversion_dict[str(os.path.abspath(label_file))] = label_file_nnunet
+        else:
+                            
+            # Repeat the above procedure for testing
+            scan_cnt_test += 1
+            # create the new convention names
+            image_file_nnunet = os.path.join(path_out_imagesTs,f'{args.taskname}_{scan_cnt_test:03d}_0000.nii.gz')
+            label_file_nnunet = os.path.join(path_out_labelsTs,f'{args.taskname}_{scan_cnt_test:03d}.nii.gz')
+
+            test_images.append(str(image_file_nnunet))
+            test_labels.append(str(label_file_nnunet))
+
+            # copy the files to new structure
+            shutil.copyfile(image_file, image_file_nnunet)
+
+            conversion_dict[str(os.path.abspath(image_file))] = image_file_nnunet
+
+    #Display of number of training and number of testing images
+    print("Number of images for training: " + str(scan_cnt_train))
+    print("Number of images for testing: " + str(scan_cnt_test))
+
+    #----------------- CREATION OF THE DICTIONNARY-----------------------------------
+    # create dataset_description.json
+    json_object = json.dumps(conversion_dict, indent=4)
+    # write to dataset description
+    conversion_dict_name = f"conversion_dict.json"
+    with open(os.path.join(path_out, conversion_dict_name), "w") as outfile:
+        outfile.write(json_object)
+
+
+    # c.f. dataset json generation. This contains the metadata for the dataset that nnUNet uses during preprocessing and training
+    # general info : https://github.com/MIC-DKFZ/nnUNet/blob/master/nnunet/dataset_conversion/utils.py
+    # example: https://github.com/MIC-DKFZ/nnUNet/blob/master/nnunet/dataset_conversion/Task055_SegTHOR.py
+
+    json_dict = OrderedDict()
+    json_dict['name'] = args.taskname
+    json_dict['description'] = args.taskname
+    json_dict['tensorImageSize'] = "3D"
+    json_dict['reference'] = "TBD"
+    json_dict['licence'] = "TBD"
+    json_dict['release'] = "0.0"
+
+    
+    # Because only using one modality  
+    ## was changed from 'modality' to 'channel_names'
+    json_dict['channel_names'] = {
+            "0": "T1w",
+        }
+    
+    # 0 is always the background. Any class labels should start from 1.
+    json_dict['labels'] = {
+        "background" : "0",
+        "MS Lesion" : "1" ,
+    }
+   
+    json_dict['numTraining'] = scan_cnt_train
+    json_dict['numTest'] = scan_cnt_test
+    #Newly required field in the json file with v2
+    json_dict["file_ending"] = ".nii.gz"
+
+    json_dict['training'] = [{'image': str(train_labels[i]).replace("labelsTr", "imagesTr") , "label": train_labels[i] }
+                                 for i in range(len(train_images))]
+    # Note: See https://github.com/MIC-DKFZ/nnUNet/issues/407 for how this should be described
+
+    #Removed because useless in this case
+    json_dict['test'] = [str(test_labels[i]).replace("labelsTs", "imagesTs") for i in range(len(test_images))]
+
+    # create dataset_description.json
+    json_object = json.dumps(json_dict, indent=4)
+    # write to dataset description
+    # nn-unet requires it to be "dataset.json"
+    dataset_dict_name = f"dataset.json"
+    with open(os.path.join(path_out, dataset_dict_name), "w") as outfile:
+        outfile.write(json_object)
