d887c09126dd0ba7347b2d54ea30b3acbc7f8044
valosekj <jan.valosek@upol.cz>
2023-07-10 15:50:52 -0400

    Fix description formatting

--- qc/generate_lesion_qc.sh
+++ qc/generate_lesion_qc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# This script brings the segmentation of the T2w image onto the STIR/PSIR space and generates lesion QC (using SCT) for STIR/PSIR images. More context in: https://github.com/ivadomed/canproco/issues/31
+# This script brings the segmentation of the T2w image onto the STIR/PSIR space and generates lesion QC (using SCT) for
+# STIR/PSIR images. More context in: https://github.com/ivadomed/canproco/issues/31
 #
 # Dependencies (versions):
 # - SCT (5.8)
